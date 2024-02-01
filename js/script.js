435c467394db1d3f8a77c4e7414b36a9a90d344f
ZanZan76 <basyirfauzan51@gmail.com>
2024-02-01 21:47:45 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const inputName = document.getElementById('input-name');
+const inputEmail = document.getElementById('input-email');
+const inputPhonenumber = document.getElementById('input-phonenumber');
+const inputMessage = document.getElementById('input-messages');
+
+const button = document.getElementById('button-submit');
